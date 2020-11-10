d4521a7e478f89a5208c17bc2cea1433d12579db
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-11-09 21:21:14 -0600

    searches based on user input

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useAppContext } from '../../context/appContext';
 import './Search.styles.css';
 
 const Search = () => {
+  const { setSearchTerm } = useAppContext();
+
   return (
     <section className='search-container'>
-      <input type='text' placeholder='Search' />
+      <input
+        onChange={(e) => setSearchTerm(e.target.value)}
+        type='text'
+        placeholder='Search'
+      />
       <div className='line'></div>
     </section>
   );
--- src/context/appContext.tsx
+++ src/context/appContext.tsx
@@ -10,16 +10,22 @@ import { State, TImage } from '../components/types/types';
 
 type TContext = {
   images: TImage[];
+  setSearchTerm: (term: string) => void;
 };
 
 const initialContext: TContext = {
   images: [],
+  setSearchTerm: (term) => null,
 };
 
 const AppContext = createContext<TContext>(initialContext);
 export const AppProvider: FC<ReactNode> = ({ children }) => {
   const [images, setImages] = useState<TImage[]>([]);
-  const unsplashUrl = `https://api.unsplash.com/search/photos?page=1&query=car&client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const unsplashUrl = `https://api.unsplash.com/search/photos?page=1&query=${
+    searchTerm || 'car'
+  }&client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 
   useEffect(() => {
     const fetchInitialImages = async () => {
@@ -31,10 +37,12 @@ export const AppProvider: FC<ReactNode> = ({ children }) => {
     };
 
     fetchInitialImages();
-  }, []);
+  }, [searchTerm]);
 
   return (
-    <AppContext.Provider value={{ images }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ images, setSearchTerm }}>
+      {children}
+    </AppContext.Provider>
   );
 };
 
